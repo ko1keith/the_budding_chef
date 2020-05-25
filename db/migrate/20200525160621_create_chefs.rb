42de90aa8c5a339226864af4a342f5ea5b05d1bd
ko.keith@hotmail.com <ko.keith@hotmail.com>
2020-05-25 12:08:07 -0400

    add recipe and chef model

---
+++ db/migrate/20200525160621_create_chefs.rb
@@ -0,0 +1,10 @@
+class CreateChefs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :chefs do |t|
+
+      t.string :chefname
+      t.string :email
+      t.timestamps
+    end
+  end
+end
