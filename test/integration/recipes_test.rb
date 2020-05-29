c1fc823ab53d412e6cdf63cab44b79862f6c1cf5
ko.keith@hotmail.com <ko.keith@hotmail.com>
2020-05-29 13:48:38 -0400

    add error message when posting invalid recipe

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -38,6 +38,14 @@ class RecipesTest < ActionDispatch::IntegrationTest
 
   test "reject invalid recipe submissions" do
     get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params:{recipe: {name:"", description: ""}}
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+
   end
 
 
