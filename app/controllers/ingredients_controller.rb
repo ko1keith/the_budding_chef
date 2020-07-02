dfd1c24d52cef27174475ab319952ad662a7e7ab
Keith <ko.keith@hotmail.com>
2020-07-02 10:03:39 -0400

    add ingredients navigation to nav bar

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,27 @@
+class IngredientsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+    
+    def show 
+    end
+
+    def index
+        @ingredients = Ingredient.paginate(page: params[:page], per_page: 5)
+    end 
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  #all ingredient routes except destroy
+  resources :ingredients, except: [:destroy]
 end
