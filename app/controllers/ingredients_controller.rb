05dd46f3681766baafde0ee8c275c323f03c568e
Keith <ko.keith@hotmail.com>
2020-07-03 10:11:33 -0400

    add create, edit ingredient page w/ admin restrictions

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,13 +1,22 @@
 class IngredientsController < ApplicationController
 
     before_action :set_ingredient, only: [:edit, :update, :show]
+    
+    #new, create, edit, update only available to admins
+    before_action :require_admin, except: [:show, :index]
 
     def new
-
+        @ingredient = Ingredient.new
     end
 
     def create
-
+        @ingredient = Ingredient.new(ingredient_params)
+        if @ingredient.save
+            flash[:success] = "Ingredient was successfully created"
+            redirect_to ingredient_path(@ingredient)
+        else
+            render 'new'
+        end
     end
 
     def edit
@@ -15,7 +24,12 @@ class IngredientsController < ApplicationController
     end
 
     def update
-
+        if @ingredient.update(ingredient_params)
+            flash[:success] = "Ingredient name was updated successfully"
+            redirect_to @ingredient
+        else
+            render 'edit'
+        end
     end
     
     def show 
@@ -28,8 +42,18 @@ class IngredientsController < ApplicationController
     end 
 
     private
+    def ingredient_params
+        params.require(:ingredient).permit(:name)
+    end
+
     def set_ingredient
         @ingredient = Ingredient.find(params[:id])
     end
 
+    def require_admin
+        if !logged_in? || (logged_in? and !current_chef.admin?)
+            flash[:danger] = "Only admin users can perform that action"
+            redirect_to ingredients_path
+        end
+    end
 end
