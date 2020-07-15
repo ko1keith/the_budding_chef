17df69da42c453cfcaea7ed9315f8a59619a356c
Keith <ko.keith@hotmail.com>
2020-07-15 09:29:26 -0400

    add recipe like div in recipes show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_135950) do
+ActiveRecord::Schema.define(version: 2020_07_15_131744) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2020_07_13_135950) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.boolean "like"
+    t.integer "chef_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "messages", force: :cascade do |t|
